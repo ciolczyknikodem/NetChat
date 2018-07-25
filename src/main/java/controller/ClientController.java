bdf8efb490da5516c47b8b6e47375a53a894ced2
AdamKruk91 <adam.kruk91@gmail.com>
2018-07-25 11:01:22 +0200

    Add ClientController with constructor

---
+++ src/main/java/controller/ClientController.java
@@ -0,0 +1,16 @@
+package controller;
+
+public class ClientController {
+
+    private final String ipAdres;
+    private final int port;
+
+    public ClientController(String ipAdres, int port) {
+        this.ipAdres = ipAdres;
+        this.port = port;
+    }
+
+    public void start(){
+        
+    }
+}
