2c24edc32de38af4ee37556a795d68728b0e5589
AdamKruk91 <adam.kruk91@gmail.com>
2018-07-25 15:43:49 +0200

    Implement conversation class

--- src/main/java/client/Conversation.java
+++ src/main/java/client/Conversation.java
@@ -1,5 +1,37 @@
 package client;
 
+import model.Client;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Conversation {
+    private ChatSender sender;
+    private ChatReciver reciver;
+    private Client author;
+    private List<Client> userList;
+    private Socket socket;
+    private ObjectOutputStream writer;
+
+    public Conversation(Socket socket, Client author, List<Client> userList) {
+        this.socket = socket;
+        this.author = author;
+        this.userList = userList;
+    }
+
+    public void start() throws IOException {
+        connectToServer();
+        sender = new ChatSender(writer, new Client("adan", "kruk"), new ArrayList<>());
+        sender.runSender();
+    }
+
+    private void connectToServer() throws IOException {
+        writer = new ObjectOutputStream(socket.getOutputStream());
+        reciver = new ChatReciver(socket);
+        reciver.runListener();
+        System.out.println("Type your message: ");
+    }
 
 }
