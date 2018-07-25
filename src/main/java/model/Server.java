424f452a52f03fc70afec019c6d87022c59b24c9
AdamKruk91 <adam.kruk91@gmail.com>
2018-07-25 11:08:17 +0200

    Add Server class and implementation with todo's

--- src/main/java/model/Server.java
+++ src/main/java/model/Server.java
@@ -1,20 +1,70 @@
 package model;
 
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
+    private final int port;
 
-    private String addressIP;
-    private int portID;
-    private Socket socket;
+    private static final List<ObjectOutputStream> clients = new ArrayList<>(); //Todo Change this to Clients?
 
-    public Server(String addressIP, int portID) {
-        this.addressIP = addressIP;
-        this.portID = portID;
+    public Server(int port) {
+        this.port = port;
     }
 
-    private void initializeSocket(String addressIP, int portID) throws IOException {
-        socket = new Socket(addressIP, portID);
+    public void startListening() throws IOException {
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+            while (true) {
+                new IncomingRequestHandler(serverSocket.accept()).start();
+            }
+
+        }
+    }
+
+    private static class IncomingRequestHandler extends Thread {
+
+        private final Socket socket;
+
+        public IncomingRequestHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+
+                synchronized (clients) {
+                    clients.add(out); // Todo Check if client in database?
+                }
+
+                while (true) {
+                    Message message = (Message) in.readObject();
+                    clients.forEach(objectOutputStream -> {
+                        try {
+                            objectOutputStream.writeObject(message); //Todo Write message to database
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                }
+
+
+            } catch (EOFException e) {
+                String userAddress = socket.getInetAddress() + ":" + socket.getPort();
+                System.out.println("User with adress: " + userAddress + " has been disconnected.");
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
