dc5276d9b3aebb2146461b18d47108b8caeeb89c
AdamKruk91 <adam.kruk91@gmail.com>
2018-07-25 11:03:57 +0200

    Add main in App for choosing user

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,24 @@
+import controller.ClientController;
+import model.Server;
+
+import java.io.IOException;
+
 public class App {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        try {
+            String typeOfUser = args[0];
+
+            if (typeOfUser.equalsIgnoreCase("server")) {
+                int port = Integer.valueOf(args[1]);
+                new Server(port).startListening();
+            } else if(typeOfUser.equalsIgnoreCase("client")){
+                String ipAddress = args[1];
+                int port = Integer.valueOf(args[2]);
+                new ClientController(ipAddress, port).start();
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Please enter correct arguments!");
+        }
     }
 }
