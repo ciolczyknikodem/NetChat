2678197f24a08b1d6379e547240136edb6ccd7fb
AdamKruk91 <adam.kruk91@gmail.com>
2018-07-25 15:53:30 +0200

    Add one conversation for client controller

--- src/main/java/client/ClientController.java
+++ src/main/java/client/ClientController.java
@@ -1,18 +1,17 @@
 package client;
 
+
 import model.Client;
 
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ClientController {
 
     private final String ipAddress;
     private final int port;
     private Socket socket;
-    private ObjectOutputStream writer;
 
     public ClientController(String ipAddress, int port) {
         this.ipAddress = ipAddress;
@@ -20,32 +19,9 @@ public class ClientController {
     }
 
     public void start() throws IOException {
-        connectToServer();
-        if(signIn()){
-            System.out.println("dupa");
-        } else {
-            System.out.println("Dupa2");
-        }
-    }
-
-    public boolean signIn(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter your login: ");
-        String userLogin = scanner.nextLine();
-        System.out.println("Please enter your password: ");
-        String userPassword = scanner.nextLine();
-        try {
-            writer.writeObject(new Client(userLogin, userPassword).toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return true;
-    }
-
-    public void connectToServer() throws IOException {
-        System.out.printf("Connecting to %s:%d ... \n", ipAddress, port);
         socket = new Socket(ipAddress, port);
-        writer = new ObjectOutputStream(socket.getOutputStream());
-
+        Client client = new Client("Adam", "KRuk");
+        Conversation conversation = new Conversation(socket, client, new ArrayList<>());
+        conversation.start();
     }
 }
